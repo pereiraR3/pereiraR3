f3d7d50c017031b23bc9f504f8e01a52f44bd7db
Anthony | Developer & Network Analytic <159745739+pereiraR3@users.noreply.github.com>
2024-07-09 11:08:31 -0400

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -6,6 +6,10 @@ const theme = 'radical';
 const streakStatsUrl = `https://github-readme-streak-stats.herokuapp.com?user=${user}&theme=${theme}&mode=weekly`;
 const topLangsUrl = `https://github-readme-stats-indol-seven-37.vercel.app/api/top-langs/?username=${user}&show_icons=true&theme=${theme}&layout=compact&token=${githubToken}`;
 
+console.log('GitHub Token:', githubToken);
+console.log('Streak Stats URL:', streakStatsUrl);
+console.log('Top Langs URL:', topLangsUrl);
+
 const readmeContent = `
 <div align="center" style="margin-bottom:100px;">
   <img width="55%" src="${streakStatsUrl}" alt="GitHub Readme Streak Stats" />
