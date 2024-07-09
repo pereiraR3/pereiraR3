8e8d74c6a7c4e013808760ecfe8febddb146fbe6
Anthony | Developer & Network Analytic <159745739+pereiraR3@users.noreply.github.com>
2024-07-09 11:50:06 -0400

    Update update-readme.js

--- update-readme.js
+++ update-readme.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const fetch = require('node-fetch');
+import fs from 'fs';
+import fetch from 'node-fetch';
 
 const githubToken = process.env.PAT_1;
 const user = 'pereiraR3';
